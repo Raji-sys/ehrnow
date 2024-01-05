84a5378d33b3f28a7ae776d8155e45f812f9181b
Muhammad Raji <rajisunusi@gmail.com>
2024-01-05 20:47:29 +0100

    first commit

---
+++ ehr/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+
+class Patient_Data(models.Models):
+    pass
+
+class Paypoint(models.Models):
+    pass
+
+class Nursing_Station(models.Models):
+    pass
+
+class Consultation(models.Models):
+    pass
+
+class Investigation(models.Models):
+    pass
+
+class Phatology(models.Models):
+    pass
+
+class Radiology(models.Models):
+    pass
+
+class Pharmacy(models.Models):
+    pass
+
+class Ward(models.Models):
+    pass
+
+class Theatre(models.Models):
+    pass
+
+class ICU(models.Models):
+    pass
