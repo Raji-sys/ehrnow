c0ef7829e3288ba52a9e3a5c1063605160dc69d3
Raji <rajisunusi@gmail.com>
2024-06-12 15:40:53 +0100

    more bugs fixed

--- pathology/models.py
+++ pathology/models.py
@@ -25,6 +25,8 @@ class HematologyTest(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
     reference_range = models.CharField(max_length=200, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"{self.name}"
