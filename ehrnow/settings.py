6e28184802054323dc628d54e6d77d3e0e43990a
Raji <rajisunusi@gmail.com>
2025-02-13 21:13:20 +0100

    Switch database engine from PostgreSQL to SQLite in settings.py

--- ehrnow/settings.py
+++ ehrnow/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'ehrnow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'emr',
-        'USER': 'raji',
-        'PASSWORD': '8080mali',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'emr',
+#         'USER': 'raji',
+#         'PASSWORD': '8080mali',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
