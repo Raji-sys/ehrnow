5b414fc904b7645ac00771c93ad4488105bf380a
Raji <rajisunusi@gmail.com>
2025-06-02 23:47:06 +0100

    static

--- ehrnow/settings.py
+++ ehrnow/settings.py
@@ -144,10 +144,10 @@ LOGOUT_REDIRECT_URL = '/logout/'
 # -----------------------------------------------------------------------------
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 # let Django + WhiteNoise serve your static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #
